c30f9a79f521640dd32b8f263e9199b9414ead1a
Lokeswara Reddy <lokireddy.posam@gmail.com>
2019-03-06 21:27:00 +0530

    components added
    Signed-off-by: Lokeswara Reddy <lokireddy.posam@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,26 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BlocksComponent } from './blocks/blocks.component';
+import { BlockComponent } from './blocks/block/block.component';
+import { BlockNavigationComponent } from './blocks/block-navigation/block-navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { GraphsComponent } from './graphs/graphs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BlocksComponent,
+    BlockComponent,
+    BlockNavigationComponent,
+    FooterComponent,
+    LoginComponent,
+    MenuComponent,
+    GraphsComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/blocks/block-navigation/block-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-block-navigation',
+    templateUrl: './block-navigation.component.html',
+    styleUrls: ['./block-navigation.component.css']
+})
+export class BlockNavigationComponent {
+
+}
