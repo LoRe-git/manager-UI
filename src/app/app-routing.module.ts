049d27fd4e5dc45bb1841feaae79f8fe47c7303d
Lokeswara Reddy <lokireddy.posam@gmail.com>
2019-03-28 19:41:59 +0530

    routing added
    Signed-off-by: Lokeswara Reddy <lokireddy.posam@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GraphsComponent } from './graphs/graphs.component';
+import { TenantsListComponent } from './tenants-list/tenants-list.component';
+import { TenantNewComponent } from './tenants-list/tenant-new/tenant-new.component';
+import { TenantUpdateComponent } from './tenants-list/tenant-update/tenant-update.component';
+import { TenantDeleteComponent } from './tenants-list/tenant-delete/tenant-delete.component';
+import { TenantSearchComponent } from './tenants-list/tenant-search/tenant-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:GraphsComponent},
+  {path:'tenants_list', component:TenantsListComponent},
+  {path:'tenant_new', component:TenantNewComponent},
+  {path:'tenant_update', component:TenantUpdateComponent},
+  {path:'tenant_delete', component:TenantDeleteComponent},
+  {path:'tenant_search', component:TenantSearchComponent},
+  {path:'**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
