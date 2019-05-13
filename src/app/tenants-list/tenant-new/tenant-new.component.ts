4e3c6af55bc2a3d4361e9feac69f51ed8e64cc6d
Lokeswara Reddy <lokireddy.posam@gmail.com>
2019-05-13 23:08:38 +0530

    form submission completed, need to add form data to tenant model

--- src/app/tenants-list/tenant-new/tenant-new.component.ts
+++ src/app/tenants-list/tenant-new/tenant-new.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { TenantService } from 'src/app/shared/service/tenant.service';
+import { Tenant } from 'src/app/shared/model/tenant.model';
 
 @Component({
   selector: 'app-tenant-new',
@@ -8,48 +10,36 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class TenantNewComponent implements OnInit {
 
-  registrationForm: FormGroup;
+  signUpForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private tenantService: TenantService) { 
+    //FormBuilder to gould form:another way to build form
+  }
 
   ngOnInit() {
-     this.registrationForm = new FormGroup({
-       'name': new FormControl(null, Validators.required),
-       id: new FormControl(''),
-       address: new FormControl(''),
-       phone: new FormControl(''),
-       roomno: new FormControl(''),
-       govid: new FormControl(''),
-       amount: new FormControl(''),
-       email: new FormControl(''),
-       dob: new FormControl(''),
-       doj: new FormControl(''),
-       blockid: new FormControl('')
-     });
+      this.signUpForm = new FormGroup({
+        'fullname': new FormControl(null, Validators.required),
+        'id': new FormControl(''),
+        'address': new FormControl(''),
+        'phone': new FormControl(''),
+        'roomno': new FormControl(''),
+        'govid': new FormControl(''),
+        'amount': new FormControl(''),
+        'email': new FormControl(''),
+        'dob': new FormControl(''),
+        'doj': new FormControl(''),
+        'blockid': new FormControl('')
+      });
 
-    // this.registrationForm = this.fb.group({
-    //   name: ['', [Validators.required]],
-    //   id: '',
-    //   address: '',
-    //   phone: '',
-    //   roomno: '',
-    //   govid: '',
-    //   amount: '',
-    //   email: '',
-    //   dob: '',
-    //   doj: '',
-    //   blockid: ''
-    // });
-
-    this.registrationForm.patchValue({
-      'name':'Loki'
+    this.signUpForm.patchValue({
+      fullname:'Loki'
     });
   }
 
-  onSubmit(regForm: FormGroup){
-    console.log(this.registrationForm);
-    console.log(regForm);
-    this.registrationForm.reset();
+  onSubmit(){
+    // this.signUpForm.get.fullname;
+    console.log(this.signUpForm);
+    this.signUpForm.reset();
   }
 
 }
