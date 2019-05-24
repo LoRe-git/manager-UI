924058d04f99a543424cdecb75daf3a83e4f9aee
Lokeswara Reddy <lokeswara.posam@ggktech.com>
2019-05-24 20:33:01 +0530

    commented un displayed fileds filter, need to add form validation
    Signed-off-by: Lokeswara Reddy <lokeswara.posam@ggktech.com>

--- src/app/shared/filter.pipe.ts
+++ src/app/shared/filter.pipe.ts
@@ -11,13 +11,13 @@ export class CustomFilterPipe implements PipeTransform{
         searchTxt = searchTxt.toLowerCase();
         return tenants.filter(tenant => {
             return tenant.t_name.toLowerCase().includes(searchTxt) 
-                    || tenant.t_amount.toString().includes(searchTxt)
-                    || tenant.t_dob.toString().includes(searchTxt)
-                    || tenant.t_doj.toString().includes(searchTxt)
-                    || tenant.t_email.toLowerCase().includes(searchTxt)
-                    || tenant.t_govId.toLowerCase().includes(searchTxt)
+                    // || tenant.t_amount.toString().includes(searchTxt)
+                    // || tenant.t_dob.toLocaleString().split(',')[0].includes(searchTxt)
+                    // || tenant.t_doj.toLocaleString().split(',')[0].includes(searchTxt)
+                    // || tenant.t_email.toLowerCase().includes(searchTxt)
+                    // || tenant.t_govId.toLowerCase().includes(searchTxt)
                     || tenant.t_phone.toString().includes(searchTxt)
-                    || tenant.t_roomno.toLowerCase().includes(searchTxt)
+                    // || tenant.t_roomno.toLowerCase().includes(searchTxt)
                     || tenant.t_address.toLowerCase().includes(searchTxt)
                     || tenant.block_id.toString().toLowerCase().includes(searchTxt) ;
         })
